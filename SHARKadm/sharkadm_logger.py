06f0f3d281a8d55773642f962d2c84399f5cef5f
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2023-09-14 11:37:02 +0200

    Store level as lowercase

--- SHARKadm/sharkadm_logger.py
+++ SHARKadm/sharkadm_logger.py
@@ -15,19 +15,21 @@ class SHARKadmLogger:
         self._transformations: dict = dict((lev, {}) for lev in self._levels)
         self._validations: dict = dict((lev, {}) for lev in self._levels)
 
-    def _check_level(self, level: str) -> None:
+    def _check_level(self, level: str) -> str:
+        level = level.lower()
         if level not in self._levels:
             msg = f'Invalid level: {level}'
             logger.error(msg)
             raise KeyError(msg)
+        return level
 
     def log_transformation(self, msg: str, level: str = 'info') -> None:
-        self._check_level(level)
+        level = self._check_level(level)
         self._transformations[level].setdefault(msg, 0)
         self._transformations[level][msg] += 1
 
     def log_validation(self, msg: str, level: str = 'info') -> None:
-        self._check_level(level)
+        level = self._check_level(level)
         self._validations[level].setdefault(msg, 0)
         self._validations[level][msg] += 1
 
