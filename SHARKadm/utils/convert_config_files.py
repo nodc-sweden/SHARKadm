f877e3939d48a2419531ba1efa2958478507934f
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2023-09-14 11:37:46 +0200

    Add translate codes

--- SHARKadm/utils/convert_config_files.py
+++ SHARKadm/utils/convert_config_files.py
@@ -44,3 +44,30 @@ def convert_column_info_to_yaml(input_path, output_path, **kwargs):
 
     with open(output_path, 'w') as fid:
         yaml.safe_dump(data, fid)
+
+
+def convert_translate_codes(input_path, output_path, **kwargs):
+    """Translates the old translate_codes.txt file to the new yaml format"""
+    data = {}
+    header = []
+    with open(input_path, encoding=kwargs.get('encoding', 'cp1252')) as fid:
+        for r, line in enumerate(fid):
+            if not line.strip():
+                continue
+            split_line = [item.strip() for item in line.split('\t')]
+            if r == 0:
+                header = split_line
+                continue
+            line_dict = dict(zip(header, split_line))
+            internal_key = line_dict['internal_key']
+            internal_value = line_dict['internal_value']
+            data.setdefault(internal_key, {})
+            data[internal_key][internal_value] = {}
+            data[internal_key][internal_value]['synonyms'] = \
+                [item.strip() for item in line_dict['synonyms'].split('<or>')]
+            for key in ['short_name', 'swedish_name', 'english_name']:
+                data[internal_key][internal_value][key] = line_dict[key]
+
+    with open(output_path, 'w') as fid:
+        yaml.safe_dump(data, fid, encoding=kwargs.get('encoding', 'cp1252'))
+
