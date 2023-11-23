8c7eb59aa0cee9c51b8cbd41e4affa47a00f47fd
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2023-11-23 10:53:53 +0100

    Refactor

--- SHARKadm/transformers/visit.py
+++ SHARKadm/transformers/visit.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from .base import Transformer, DataHolderProtocol
 
 
@@ -11,23 +9,7 @@ class AddVisitKey(Transformer):
         return f'Adds visit key column'
 
     def _transform(self, data_holder: DataHolderProtocol) -> None:
-        data_holder.data = data_holder.data.assign(
-            visit_key=(
-                    data_holder.data['datetime'].apply(lambda x: x.strftime('%Y%m%d_%H%M')) +
-                    '_' +
-                    data_holder.data['platform_code'] +
-                    '_' +
-                    data_holder.data['visit_id']
-            )
-        )
-
-        # data_holder.data['visit_key'] = pd.concat([
-        #     data_holder.data['datetime'].apply(lambda x: x.strftime('%Y%m%d_%H%M')),
-        #     data_holder.data['platform_code'],
-        #     data_holder.data['visit_id']
-        # ], axis=1, ignore_index=True).astype(str).agg('_'.join, axis=1)
-
-
-        # data_holder.data['visit_key'] = data_holder.data['datetime'].apply(lambda x: x.strftime('%Y%m%d_%H%M')) + '_' \
-        #                                 + '_' + data_holder.data['platform_code'] + '_' + data_holder.data['visit_id']
+        data_holder.data['visit_key'] = data_holder.data['datetime'].apply(lambda x: x.strftime('%Y%m%d_%H%M')).str.cat(
+            data_holder.data['platform_code'], '_'
+            ).str.cat(data_holder.data['visit_id'], '_')
 
