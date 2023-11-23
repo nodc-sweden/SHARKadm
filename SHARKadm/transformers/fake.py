73016ffe690588f276a7d4a7067fc0da51d0e816
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2023-11-23 10:48:42 +0100

    Added class

--- SHARKadm/transformers/fake.py
+++ SHARKadm/transformers/fake.py
@@ -1,7 +1,7 @@
 from .base import Transformer, DataHolderProtocol
 
 
-class FakeAddPressure(Transformer):
+class FakeAddPressureFromDepth(Transformer):
     valid_data_holders = ['LimsDataHolder']
 
     @staticmethod
@@ -11,4 +11,25 @@ class FakeAddPressure(Transformer):
     def _transform(self, data_holder: DataHolderProtocol) -> None:
         data_holder.data['PRES'] = data_holder.data['DEPH']
         data_holder.data['Q_PRES'] = data_holder.data['Q_DEPH']
+
+
+class FakeAddCTDtagToColumns(Transformer):
+    valid_data_holders = ['LimsDataHolder']
+
+    @staticmethod
+    def get_transformer_description() -> str:
+        return f'Adds _CTD to all parameter columns. This is so that data can be compatible with the cdtvis bokeh ' \
+               f'visualization. '
+
+    def _transform(self, data_holder: DataHolderProtocol) -> None:
+        new_column = []
+        for col in data_holder.data.columns:
+            if 'CTD' in col:
+                new_column.append(col)
+                continue
+            if col.startswith(('Q_', 'Q0_')) or f'Q_{col}' in data_holder.data.columns:
+                new_column.append(f'{col}_CTD')
+            else:
+                new_column.append(col)
+        data_holder.data.columns = new_column
 
