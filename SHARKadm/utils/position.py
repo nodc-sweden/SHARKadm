cb19ff704b27059c1c9f3923105a3fd97eed0b04
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2023-11-23 11:19:47 +0100

    Remove default nr_decimals

--- SHARKadm/utils/position.py
+++ SHARKadm/utils/position.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def decdeg_to_decmin(pos: float | str, nr_decimals: int = 2, with_space: bool = False):
+def decdeg_to_decmin(pos: float | str, nr_decimals: int | None = 2, with_space: bool = False):
     pos = float(pos)
     deg = np.floor(pos)
     minute = pos % deg * 60.0
@@ -15,7 +15,7 @@ def decdeg_to_decmin(pos: float | str, nr_decimals: int = 2, with_space: bool = 
     return output
 
 
-def decmin_to_decdeg(pos: str, nr_decimals: int = 2):
+def decmin_to_decdeg(pos: str, nr_decimals: int | None = 2):
     pos = float(pos.replace(' ', ''))
     if pos >= 0:
         output = np.floor(pos / 100.) + (pos % 100) / 60.
