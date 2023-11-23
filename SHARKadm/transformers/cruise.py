ee88eb1f993d9d8416a1b42045f6769c5d00689c
magnuswenzerSMHI <magnus.wenzer@smhi.se>
2023-11-23 10:48:17 +0100

    refactor

--- SHARKadm/transformers/cruise.py
+++ SHARKadm/transformers/cruise.py
@@ -2,20 +2,12 @@ from .base import Transformer, DataHolderProtocol
 
 
 class AddCruiseId(Transformer):
+    valid_data_types = ['physicalchemical']
 
     @staticmethod
     def get_transformer_description() -> str:
         return f'Adds cruise id column'
 
     def _transform(self, data_holder: DataHolderProtocol) -> None:
-        data_holder.data = data_holder.data.assign(
-            visit_key=(
-                    data_holder.data['visit_year'] +
-                    '_' +
-                    data_holder.data['platform_code'] +
-                    '_' +
-                    data_holder.data['cruise_no']
-            )
-        )
-        # data_holder.data['cruise_id'] = data_holder.data['visit_year'] + '_' + data_holder.data['platform_code'] + '_' + data_holder.data['cruise_no']
-
+        data_holder.data['cruise_id'] = data_holder.data['visit_year'].str.cat(data_holder.data['platform_code'], '_'
+                                                                               ).str.cat(data_holder.data['cruise_no'], '_')
