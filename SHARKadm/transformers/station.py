4d7814e1201e01e2339826dc9a2a98c2c2e0e2f4
Wenzer Magnus <a001985@ad.smhi.se>
2024-02-21 18:59:27 +0100

    Speedup

--- SHARKadm/transformers/station.py
+++ SHARKadm/transformers/station.py
@@ -4,7 +4,10 @@ from .base import Transformer, DataHolderProtocol
 
 
 class AddStationInfo(Transformer):
-    columns_to_set = ['station_name', 'station_id', 'sample_location_id']
+    source_lat_column = 'sample_latitude_dd'
+    source_lon_column = 'sample_longitude_dd'
+    reported_station_col = 'reported_station_name'
+    columns_to_set = ['station_name', 'station_id', 'sample_location_id', 'station_viss_eu_id']
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -19,13 +22,17 @@ class AddStationInfo(Transformer):
     def _transform(self, data_holder: DataHolderProtocol) -> None:
         self._create_columns_if_missing(data_holder)
 
-        for i in data_holder.data.index:
-            reported_station = data_holder.data.at[i, 'reported_station_name']
-            # translated_station_name = self._station_synonyms.setdefault(reported_station,
-            #                                                             self._stations.get_station_name(
-            #                                                                 reported_station))
-            lat = float(data_holder.data.at[i, 'sample_latitude_dd'])
-            lon = float(data_holder.data.at[i, 'sample_longitude_dd'])
+        for (lat_str, lon_str, reported_station), df in data_holder.data.groupby([self.source_lon_column, self.source_lon_column, self.reported_station_col]):
+            if not (lat_str and lon_str):
+                adm_logger.log_transformation(f'Missing {self.source_lat_column} and/or {self.source_lon_column} in {self.__class__.__name__}',
+                                              level=adm_logger.WARNING)
+                continue
+
+            boolean = (data_holder.data[self.source_lat_column] == lat_str) & (data_holder.data[self.source_lon_column] == lon_str) & (data_holder.data[self.reported_station_col] == reported_station)
+
+            lat = float(lat_str)
+            lon = float(lon_str)
+
             info = self._loaded_stations_info.setdefault(reported_station,
                                                          self._stations.get_closest_station_info(lat, lon))
             if not info['accepted']:
@@ -35,10 +42,40 @@ class AddStationInfo(Transformer):
 
             if reported_station != info['STATION_NAME']:
                 adm_logger.log_transformation(f'Station name translated: {reported_station} -> {info['STATION_NAME']}', level='warning')
-            data_holder.data.at[i, 'station_name'] = info['STATION_NAME']
-            data_holder.data.at[i, 'station_id'] = info['REG_ID_GROUP']
-            data_holder.data.at[i, 'sample_location_id'] = info['REG_ID']
-            data_holder.data.at[i, 'station_viss_eu_id'] = info['EU_CD']
+            data_holder.data.loc[boolean, 'station_name'] = info['STATION_NAME']
+            data_holder.data.loc[boolean, 'station_id'] = info['REG_ID_GROUP']
+            data_holder.data.loc[boolean, 'sample_location_id'] = info['REG_ID']
+            data_holder.data.loc[boolean, 'station_viss_eu_id'] = info['EU_CD']
+
+    # def _transform(self, data_holder: DataHolderProtocol) -> None:
+    #     self._create_columns_if_missing(data_holder)
+    #
+    #     for i in data_holder.data.index:
+    #         reported_station = data_holder.data.at[i, 'reported_station_name']
+    #         # translated_station_name = self._station_synonyms.setdefault(reported_station,
+    #         #                                                             self._stations.get_station_name(
+    #         #                                                                 reported_station))
+    #         lat_str = data_holder.data.at[i, self.source_lat_column]
+    #         lon_str = data_holder.data.at[i, self.source_lon_column]
+    #         if not (lat_str and lon_str):
+    #             adm_logger.log_transformation(f'Missing {self.source_lat_column} and/or {self.source_lon_column} in {self.__class__.__name__}',
+    #                                           level=adm_logger.WARNING)
+    #             continue
+    #         lat = float(lat_str)
+    #         lon = float(lon_str)
+    #         info = self._loaded_stations_info.setdefault(reported_station,
+    #                                                      self._stations.get_closest_station_info(lat, lon))
+    #         if not info['accepted']:
+    #             adm_logger.log_transformation(f'Reported position is to far from position in station list: '
+    #                                           f'{reported_station} ({lat} / {lon}); {info["calc_dist"]} ({info["OUT_OF_BOUNDS_RADIUS"]})')
+    #             continue
+    #
+    #         if reported_station != info['STATION_NAME']:
+    #             adm_logger.log_transformation(f'Station name translated: {reported_station} -> {info['STATION_NAME']}', level='warning')
+    #         data_holder.data.at[i, 'station_name'] = info['STATION_NAME']
+    #         data_holder.data.at[i, 'station_id'] = info['REG_ID_GROUP']
+    #         data_holder.data.at[i, 'sample_location_id'] = info['REG_ID']
+    #         data_holder.data.at[i, 'station_viss_eu_id'] = info['EU_CD']
 
     def _create_columns_if_missing(self, data_holder: DataHolderProtocol) -> None:
         for col in self.columns_to_set:
