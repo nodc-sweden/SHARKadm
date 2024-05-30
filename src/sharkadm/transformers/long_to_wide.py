26018f75f7183ed3fe205733f38d6d5773e759bf
Wenzer Magnus <a001985@ad.smhi.se>
2024-05-30 10:43:15 +0200

    Cleanup prints

--- src/sharkadm/transformers/long_to_wide.py
+++ src/sharkadm/transformers/long_to_wide.py
@@ -16,15 +16,12 @@ class LongToWide(Transformer):
         meta_columns = [col for col in data_holder.data.columns if col not in not_meta_columns]
 
         import time
-        t0 = time.time()
-        merge_key = 'shark_sample_id'
+#        t0 = time.time()
         merge_key = ['visit_date', 'sample_time', 'sample_depth_m']
-        unique_keys = sorted(set(data_holder.data[merge_key]))
         unique_pars = sorted(set(data_holder.data['parameter']))
         unit_mapping = {}
         data_list = []
         for key, key_df in data_holder.data.groupby(merge_key):
-            # print(key)
             meta_data = []
             par_data = []
             for meta_col in meta_columns:
@@ -50,15 +47,9 @@ class LongToWide(Transformer):
             else:
                 qpar = f'QFLAG.{par}'
                 par = f'{par}.{unit}'
-                print(f'{par=}   :   {unit=}')
-                #if unit:
-                #    par = f'{par}.{unit}'
-            # elif par not in data_holder.header_mapper.external_parameters:
-            #     print(f'==== {par=}')
-            #     par = f'{par}.{unit}'
             par_columns.append(par)
             par_columns.append(qpar)
 
         data_holder.data = pd.DataFrame(data=data_list, columns=meta_columns + par_columns)
-        print(time.time() - t0)
+#        print(time.time() - t0)
 
