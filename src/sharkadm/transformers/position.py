3a32a325166f24746bbbf6e1b17e2d36b481a0b0
Wenzer Magnus <a001985@ad.smhi.se>
2024-06-25 15:50:27 +0200

    Cleanup

--- src/sharkadm/transformers/position.py
+++ src/sharkadm/transformers/position.py
@@ -54,13 +54,10 @@ class AddSamplePosition(Transformer):
             return '', ''
 
         if self._is_sweref99tm(value=lat_value, info=self.lat_info) and self._is_sweref99tm(value=lon_value, info=self.lon_info):
-            print(f'{geography.sweref99tm_to_decdeg(lon_value, lat_value)=}')
             return geography.sweref99tm_to_decdeg(lon_value, lat_value)
         elif self._is_dm_lat(lat_value) and self._is_dm_lon(lon_value):
-            print(f'{geography.decmin_to_decdeg(lat_value), geography.decmin_to_decdeg(lon_value)=}')
             return geography.decmin_to_decdeg(lat_value), geography.decmin_to_decdeg(lon_value)
         elif self._is_dd(lat_value) and self._is_dd(lon_value):
-            print(f'{lat_value, lon_value=}')
             return lat_value, lon_value
 
     def _is_sweref99tm(self, value: str, info: dict) -> bool:
