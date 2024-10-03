4c0ddb9d3fb30559e6ca76de3906d39a488703df
a001985 <magnus.wenzer@smhi.se>
2024-10-03 12:54:13 +0200

    Cleanup

--- src/sharkadm/config_paths.py
+++ src/sharkadm/config_paths.py
@@ -15,21 +15,21 @@ LOCAL_CONFIG_ROOT_DIR = THIS_DIR / 'CONFIG_FILES'
 
 class ConfigPaths:
 
-    def __init__(self, root_path: str | pathlib.Path):
+    def __init__(self, root_path: str | pathlib.Path) -> None:
         self._root_path = pathlib.Path(root_path)
         self._paths = dict()
         self._save_paths()
 
-    def __call__(self, item: str) -> pathlib.Path:
+    def __call__(self, item: str) -> pathlib.Path | None:
         return self._paths.get(item, None)
 
-    def __getattr__(self, item: str) -> pathlib.Path:
+    def __getattr__(self, item: str) -> pathlib.Path | None:
         return self._paths.get(item, None)
 
-    def __getitem__(self, item: str) -> pathlib.Path:
+    def __getitem__(self, item: str) -> pathlib.Path | None:
         return self._paths.get(item, None)
 
-    def _save_paths(self):
+    def _save_paths(self) -> None:
         for path in self._root_path.iterdir():
             self._paths[path.stem] = path
 
