239f0d60018e2cc58c1b5eb70c07524e9bcf92bb
a001985 <magnus.wenzer@smhi.se>
2024-10-22 15:48:22 +0200

    Add calculation and update bvol module

--- src/sharkadm/transformers/calculate.py
+++ src/sharkadm/transformers/calculate.py
@@ -13,7 +13,21 @@ class Occurrence:
 
     @property
     def coefficient(self):
-        return int(list(self._df['coefficient'])[0])
+        return int(self._df['coefficient'].values[0])
+
+    @property
+    def reported_cell_volume_um3(self):
+        volume = self._df['reported_cell_volume_um3'].values[0]
+        if not volume:
+            return
+        return int(volume)
+
+    @property
+    def reported_cell_volume_mm3(self):
+        cell_volume_um3 = self.reported_cell_volume_um3
+        if not cell_volume_um3:
+            return
+        return cell_volume_um3 * 10 ** -9
 
 
 class Calculate(Transformer):
@@ -24,5 +38,23 @@ class Calculate(Transformer):
         return f'Calculate abundance and other stuf'
 
     def _transform(self, data_holder: DataHolderProtocol) -> None:
+        series_to_add = []
         for _id, df in data_holder.data.groupby(self.occurrence_id_col):
             occ = Occurrence(df)
+            series = df.loc[df.index[0]].squeeze()
+
+            calc_abundance = occ.counted * occ.coefficient
+            series['CALC_Abundance'] = str(calc_abundance)
+            series_to_add.append(series)
+
+
+            # Från JAVA-kod biovol
+            # class BvolObject {
+            #
+            # private String referenceList = "";
+            # private String scientificName = "";
+            # private String aphiaId = "";
+            # private String sizeClass = "";
+            # private String trophicType = "";
+            # private String calculatedVolume = "";
+            # private String calculatedCarbon = "";
