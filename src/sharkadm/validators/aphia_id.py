a0897e0f36c9ad22d053ccf49d838c8cd82050a7
a001985 <magnus.wenzer@smhi.se>
2024-10-31 14:11:51 +0100

    Add compare

--- src/sharkadm/validators/aphia_id.py
+++ src/sharkadm/validators/aphia_id.py
@@ -1,37 +1,65 @@
 from .base import Validator, DataHolderProtocol
 
 from sharkadm import adm_logger
-import logging
-
-logger = logging.getLogger(__name__)
 
 
-class ValidateAphiaId(Validator):
-    reported_aphia_id_col = 'reported_aphia_id'
-    aphia_id_col = 'aphia_id'
+class _ValidateAphiaId(Validator):
+    from_aphia_id_col = ''
+    to_aphia_id_col = ''
 
     @staticmethod
     def get_validator_description() -> str:
-        return f'Checks if translated {ValidateAphiaId.aphia_id_col} is the same as {ValidateAphiaId.reported_aphia_id_col}'
+        return ''
 
     def _validate(self, data_holder: DataHolderProtocol) -> None:
-        if not all([col in data_holder.data for col in [self.reported_aphia_id_col, self.aphia_id_col]]):
-            adm_logger.log_validation(f'One or more columns missing in to execute {self.__class__.__name__}', level=adm_logger.WARNING)
+        if self.from_aphia_id_col not in data_holder.data:
+            adm_logger.log_validation(f'Could not validate aphia_id. Missing column {self.from_aphia_id_col}',
+                                      level=adm_logger.WARNING)
             return
 
-        if not any(data_holder.data[self.reported_aphia_id_col]):
-            adm_logger.log_validation(f'No values in {self.reported_aphia_id_col}')
+        if self.to_aphia_id_col not in data_holder.data:
+            adm_logger.log_validation(f'Could not validate aphia_id. Missing column {self.to_aphia_id_col}',
+                                      level=adm_logger.WARNING)
             return
 
-        for (reported, translated), df in data_holder.data.groupby([self.reported_aphia_id_col, self.aphia_id_col]):
+        if not any(data_holder.data[self.from_aphia_id_col]):
+            adm_logger.log_validation(f'No values in {self.from_aphia_id_col}')
+            return
+
+        for (reported, translated), df in data_holder.data.groupby([self.from_aphia_id_col, self.to_aphia_id_col]):
+            if not reported:
+                adm_logger.log_validation(f'Missing {self.from_aphia_id_col} ({len(df)} places)')
+            if not translated:
+                adm_logger.log_validation(f'Missing {self.to_aphia_id_col} ({len(df)} places)')
+            if not (reported and translated):
+                continue
             if reported == translated:
                 continue
-            if not reported:
-                if translated:
-                    adm_logger.log_validation(f'{self.aphia_id_col} {translated} added {len(df)} times')
-                    continue
-                adm_logger.log_validation(f'Missing {self.aphia_id_col} in {len(df)} row')
-                continue
-            adm_logger.log_validation(f'{self.aphia_id_col} differs from reported: {reported} -> {translated} ({len(df)} times)')
+            adm_logger.log_validation(f'{self.to_aphia_id_col} differs from {self.from_aphia_id_col}: "{reported}" ({self.from_aphia_id_col}) <-> "{translated}" ({self.to_aphia_id_col}) ({len(df)} places)')
 
 
+class ValidateReportedVsAphiaId(_ValidateAphiaId):
+    from_aphia_id_col = 'reported_aphia_id'
+    to_aphia_id_col = 'aphia_id'
+
+    @staticmethod
+    def get_validator_description() -> str:
+        return f'Checks if {ValidateReportedVsAphiaId.to_aphia_id_col} is the same as {ValidateReportedVsAphiaId.from_aphia_id_col}'
+
+
+class ValidateReportedVsBvolAphiaId(_ValidateAphiaId):
+    from_aphia_id_col = 'reported_aphia_id'
+    to_aphia_id_col = 'bvol_aphia_id'
+
+    @staticmethod
+    def get_validator_description() -> str:
+        return f'Checks if {ValidateReportedVsBvolAphiaId.to_aphia_id_col} is the same as {ValidateReportedVsBvolAphiaId.from_aphia_id_col}'
+
+
+class ValidateAphiaIdVsBvolAphiaId(_ValidateAphiaId):
+    from_aphia_id_col = 'aphia_id'
+    to_aphia_id_col = 'bvol_aphia_id'
+
+    @staticmethod
+    def get_validator_description() -> str:
+        return f'Checks if {ValidateAphiaIdVsBvolAphiaId.to_aphia_id_col} is the same as {ValidateAphiaIdVsBvolAphiaId.from_aphia_id_col}'
