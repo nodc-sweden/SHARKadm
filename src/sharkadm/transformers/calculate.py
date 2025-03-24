77120068d0b35888fa0028a3366eb30b918c842a
a001985 <magnus.wenzer@smhi.se>
2025-03-24 17:42:05 +0100

    More calculations

--- src/sharkadm/transformers/calculate.py
+++ src/sharkadm/transformers/calculate.py
@@ -4,6 +4,17 @@ from .base import Transformer, DataHolderProtocol
 import pandas as pd
 from sharkadm import adm_logger
 
+
+try:
+    from nodc_bvol import get_bvol_nomp_object
+    _nomp = get_bvol_nomp_object()
+except ModuleNotFoundError as e:
+    _nomp = None
+    module_name = str(e).split("'")[-2]
+    adm_logger.log_workflow(f'Could not import package "{module_name}" in module {__name__}. You need to install this dependency if you want to use this module.', level=adm_logger.WARNING)
+
+
+
 #
 # class PhytoplanktonOccurrence:
 #
@@ -174,6 +185,7 @@ from sharkadm import adm_logger
             # private String calculatedCarbon = "";
 
 
+
 class CalculateAbundance(Transformer):
     count_col = 'COPY_VARIABLE.# counted.ind/analysed sample fraction'
     coef_col = 'coefficient'
@@ -190,6 +202,79 @@ class CalculateAbundance(Transformer):
             adm_logger.log_transformation(f'Could not calculate {CalculateAbundance.parameter}. Missing {self.col_must_exist} column',
                                           level=adm_logger.ERROR)
             return
+        boolean = (data_holder.data[self.count_col] != '') & (data_holder.data[self.coef_col] != '') & (data_holder.data['parameter'] == self.parameter)
+        data_holder.data.loc[boolean, 'calculated_value'] = (data_holder.data.loc[boolean, self.count_col].astype(float) * data_holder.data.loc[boolean, self.coef_col].astype(float)).round(1)
+
+
+class CalculateBiovolume(Transformer):
+    size_class_col = 'size_class'
+    aphia_id_col = 'aphia_id'
+    reported_cell_volume_col = 'reported_cell_volume_um3'
+    col_must_exist = 'reported_value'
+    parameter = 'Biovolume concentration'
+    col_to_set = 'calculated_value'
+
+    @staticmethod
+    def get_transformer_description() -> str:
+        return f'Calculating {CalculateBiovolume.parameter}. Setting value to column {CalculateBiovolume.col_to_set}'
+
+    def _transform(self, data_holder: DataHolderProtocol) -> None:
+        if self.col_must_exist not in data_holder.data:
+            adm_logger.log_transformation(f'Could not calculate {CalculateBiovolume.parameter}. Missing {self.col_must_exist} column',
+                                          level=adm_logger.ERROR)
+            return
+        for (aphia_id, size_class), df in data_holder.data.groupby([self.aphia_id_col, self.size_class_col]):
+            if not aphia_id:
+                adm_logger.log_transformation(
+                    f'Could not calculate {CalculateBiovolume.parameter}. Missing aphia_id',
+                    level=adm_logger.WARNING)
+                continue
+            if not size_class:
+                adm_logger.log_transformation(
+                    f'Could not calculate {CalculateBiovolume.parameter}. Missing size_class',
+                    level=adm_logger.WARNING)
+                continue
+
+            cell_volume = self._get_bvol_cell_volume(aphia_id, size_class)
+            if not cell_volume:
+                cell_volume = self._get_reported_cell_volume(df, aphia_id, size_class)
+            if not cell_volume:
+                adm_logger.log_transformation(
+                    f'Could not calculate {CalculateBiovolume.parameter}. No cell volume in nomp or in data for aphia_id="{aphia_id}" and size_class="{size_class}"',
+                    level=adm_logger.WARNING)
+                continue
+
+
+    def _get_bvol_cell_volume(self, aphia_id: int | str, size_class: int | str) -> float | None:
+        info = _nomp.get_info(AphiaID=str(aphia_id), SizeClassNo=str(size_class))
+        use_volume = None
+        if not info:
+            adm_logger.log_transformation(
+                f'Could not calculate {CalculateBiovolume.parameter} with aphia_id="{aphia_id}" and size_class="{size_class}"',
+                level=adm_logger.WARNING)
+        else:
+            volume = info.get('Calculated_volume_µm3')  # This is in um^3
+            if volume is None:
+                adm_logger.log_transformation(
+                    f'Could not calculate {CalculateBiovolume.parameter}. No Calculated_volume_µm3 found in nomp list for aphia_id="{aphia_id}" and size_class="{size_class}"',
+                    level=adm_logger.WARNING)
+            else:
+                use_volume = volume.replace(',', '.')
+        if not use_volume:
+            return
+        return float(use_volume) * 10e-9
+
+    def _get_reported_cell_volume(self, df: pd.DataFrame, aphia_id: int, size_class: int) -> float | None:
+        values = set(df[self.reported_cell_volume_col])
+        if len(values) != 1:
+            adm_logger.log_transformation(
+                f'Could not calculate {CalculateBiovolume.parameter}. {self.reported_cell_volume_col} has several values for aphia_id="{aphia_id}" and size_class="{size_class}"',
+                level=adm_logger.WARNING)
+            return
+        return float(values.pop()) * 10e-9
+
+
+
         boolean = (data_holder.data[self.count_col] != '') & (data_holder.data[self.coef_col] != '') & (data_holder.data['parameter'] == self.parameter)
         data_holder.data.loc[boolean, 'calculated_value'] = (data_holder.data.loc[boolean, self.count_col].astype(float) * data_holder.data.loc[boolean, self.coef_col].astype(float)).round(1)
 
