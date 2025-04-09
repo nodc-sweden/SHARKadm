21cacb57b7bae9b8ad8d11321bd1242089cda095
a001985 <magnus.wenzer@smhi.se>
2025-04-09 10:34:24 +0200

    Fix conflict

--- src/sharkadm/__init__.py
+++ src/sharkadm/__init__.py
@@ -2,16 +2,19 @@ import subprocess
 
 from sharkadm.sharkadm_exceptions import ConfigurationError
 from sharkadm.utils import get_nodc_config_directory
+from sharkadm.sharkadm_logger import adm_logger
 
 
 def update_nodc_config_directory_from_svn() -> None:
     config_dir = get_nodc_config_directory()
     if not config_dir:
-        print("No nodc_config directory is configured.")
+        adm_logger.log_workflow(
+            "No nodc_config directory is configured.", level=adm_logger.WARNING
+        )
         return
     status = subprocess.run(["svn", "update"], cwd=config_dir)
     if status.returncode:
-        raise ConfigurationError("Could not update nodc_config.")
+        adm_logger.log_workflow("Could not update nodc_config.", level=adm_logger.WARNING)
 
 
 update_nodc_config_directory_from_svn()
