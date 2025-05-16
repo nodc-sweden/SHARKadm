d73f5d9b4c59f72ab8afae38a985d070c52e48ed
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2025-05-16 16:37:18 +0200

    Positive validator
    Checking that specific columns only contain strictly positive values.
    Also:
    - Changing behavior for row numbers in logs. Call log method with
      either row_number or row_numbers (or both). They will be consolidated
      to list of int.

--- src/sharkadm/validators/positive.py
+++ src/sharkadm/validators/positive.py
@@ -1,10 +1,5 @@
-import logging
-
 from sharkadm.sharkadm_logger import adm_logger
-
-from .base import DataHolderProtocol, Validator
-
-logger = logging.getLogger(__name__)
+from sharkadm.validators.base import DataHolderProtocol, Validator
 
 
 class ValidatePositiveValues(Validator):
@@ -30,16 +25,26 @@ class ValidatePositiveValues(Validator):
         for col in self.cols_to_validate:
             if col not in data_holder.data:
                 continue
-            adm_logger.log_validation_failed(
+            self._log_workflow(
                 f"Checking that all values are positive in column {col}",
                 level=adm_logger.DEBUG,
             )
+            error = False
             for val, df in data_holder.data.groupby(col):
                 if not val:
                     continue
                 if float(val) < 0:
-                    adm_logger.log_validation_failed(
+                    self._log_fail(
                         f"Negative values found in colum {col} LINES: "
                         f"{sorted(df['row_number'])}",
+                        column=col,
+                        row_numbers=list(df["row_number"]),
                         level=adm_logger.WARNING,
                     )
+                    error = True
+            if not error:
+                self._log_success(
+                    "No negative values found in column {col}.",
+                    column=col,
+                    level=adm_logger.INFO,
+                )
