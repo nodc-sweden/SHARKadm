7109a17db7b145ff5377b02efd78782a1b6b5b7b
Jonatan Skogsfors <jonatan.skogsfors@smhi.se>
2025-08-07 12:51:46 +0200

    Date and time validation
    Part of #175

--- tests/validators/conftest.py
+++ tests/validators/conftest.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import polars as pl
 import pytest
 
-from sharkadm.data import PandasDataHolder
+from sharkadm.data import PandasDataHolder, PolarsDataHolder
 
 
 class PandasDataFrameHolder(PandasDataHolder):
@@ -25,6 +26,32 @@ class PandasDataFrameHolder(PandasDataHolder):
         return "data_holder_description"
 
 
+class PolarsDataFrameHolder(PolarsDataHolder):
+    def __init__(self, data: pl.DataFrame):
+        super().__init__()
+        self._data = data
+
+    @property
+    def data_type(self) -> str:
+        return "data_type"
+
+    @property
+    def data_type_internal(self) -> str:
+        return "data_type_internal"
+
+    @property
+    def dataset_name(self) -> str:
+        return "dataset_name"
+
+    def get_data_holder_description(self) -> str:
+        return "data_holder_description"
+
+
 @pytest.fixture
 def pandas_data_frame_holder_class():
     return PandasDataFrameHolder
+
+
+@pytest.fixture
+def polars_data_frame_holder_class():
+    return PolarsDataFrameHolder
