d1d54d3a06562323acabf6a0509d15fe8c58c1d5
Ylva Ericson <ylva.ericson@smhi.se>
2026-03-09 13:56:03 +0100

    add int columns

--- src/sharkadm/utils/add_column.py
+++ src/sharkadm/utils/add_column.py
@@ -17,3 +17,21 @@ def add_float_column(
         .cast(float)
         .alias(new_column_name)
     )
+
+
+def add_int_column(
+    data: pl.DataFrame, column: str, column_name: str = "", suffix: str = "int"
+) -> pl.DataFrame:
+    new_column_name = column_name
+    if not new_column_name:
+        suffix = suffix.strip("_")
+        new_column_name = f"{column}_{suffix}"
+    if column not in data.columns:
+        return data.with_columns(pl.lit(None).cast(int).alias(new_column_name))
+    return data.with_columns(
+        pl.when(pl.col(column).str.len_chars() == 0)
+        .then(None)
+        .otherwise(pl.col(column))
+        .cast(int)
+        .alias(new_column_name)
+    )
