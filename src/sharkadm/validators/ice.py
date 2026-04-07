03e78f0e3d725972225dbfb49ed7586e16762dee
Ylva Ericson <ylva.ericson@smhi.se>
2026-04-07 17:12:13 +0200

    ice obs validator fix

--- src/sharkadm/validators/ice.py
+++ src/sharkadm/validators/ice.py
@@ -25,36 +25,56 @@ class ValidateIceob(Validator):
             or "reported_station_name" not in data_holder.data.columns
         ):
             self._log_fail("Missing visit date or reported station name columns.")
+            return
 
         valid_values = [
             str(i) for i in range(10) if i not in (2, 3)
         ]  # 2, 3 refers to icebergs
-        unique_rows = data_holder.data.select(
-            ["visit_date", "reported_station_name", "ice_observation_code"]
-        ).unique()
+        unique_rows = (
+            data_holder.data.select(
+                [
+                    "visit_date",
+                    "reported_station_name",
+                    "ice_observation_code",
+                    "row_number",
+                ]
+            )
+            .group_by(["visit_date", "reported_station_name", "ice_observation_code"])
+            .agg(pl.col("row_number").alias("row_numbers"))
+        )
         unique_rows = unique_rows.with_columns(
-            [
-                pl.when(
-                    pl.col("ice_observation_code").is_null()
-                    | (pl.col("ice_observation_code") == "")
+            pl.when(pl.col("ice_observation_code").is_in(valid_values))
+            .then(pl.lit("Ice observation code is ok"))
+            .when(
+                pl.col("ice_observation_code").is_null()
+                | (pl.col("ice_observation_code").str.strip_chars() == "")
+            )
+            .then(
+                pl.format(
+                    "{} on {}: Missing ice observation code: {}",
+                    pl.col("reported_station_name"),
+                    pl.col("visit_date"),
+                    pl.col("ice_observation_code"),
                 )
-                .then(True)
-                .when(pl.col("ice_observation_code").is_in(valid_values))
-                .then(True)
-                .otherwise(False)
-                .alias("is_valid")
-            ]
+            )
+            .otherwise(
+                pl.format(
+                    "{} on {}: Ice observation code has unexpected value: {}",
+                    pl.col("reported_station_name"),
+                    pl.col("visit_date"),
+                    pl.col("ice_observation_code"),
+                )
+            )
+            .alias("message")
         )
 
-        if unique_rows["is_valid"].all():
-            self._log_success("Ice observation code is ok")
+        if (
+            unique_rows.filter(pl.col("message") != "Ice observation code is ok").height
+            == 0
+        ):
+            self._log_success("All ice observation codes are ok")
         else:
-            erroneous_rows = (
-                unique_rows.filter(~pl.col("is_valid"))
-                .select(["visit_date", "reported_station_name", "ice_observation_code"])
-                .to_dicts()
-            )
-
-            self._log_fail(
-                f"Ice observation code has unexpected values:\n{erroneous_rows}"
-            )
+            for (msg,), df in unique_rows.filter(
+                pl.col("message") != "Ice observation code is ok"
+            ).group_by("message"):
+                self._log_fail(msg=msg, row_numbers=df["row_numbers"][0])
