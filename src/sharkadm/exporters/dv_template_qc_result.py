3754feb4a657ef0a46426a6483f7f3f71619de28
Maria-Pia Birkett <a002572@ad.smhi.se>
2026-06-18 10:18:43 +0200

    Added exporter for LIMS input file

--- src/sharkadm/exporters/dv_template_qc_result.py
+++ src/sharkadm/exporters/dv_template_qc_result.py
@@ -1,6 +1,7 @@
 import pathlib
 
 import openpyxl
+import polars as pl
 from openpyxl.comments import Comment
 from openpyxl.utils import get_column_letter
 
@@ -76,5 +77,85 @@ class ExportDvTemplateWithQcResult(PolarsFileExporter):
                 f"{missing_parameters}"
             )
 
-        indata.save(self.export_file_path)  # Savea som ny fil
+        indata.save(self.export_file_path)
+        print("TOTAL_QC-kolumner har lagts till och fyllts i Excel-filen.")
+
+
+class LimsExportDvTemplateWithQcResult(PolarsFileExporter):
+    def __init__(self, lims_template_file, **kwargs):
+        self._lims_template_file = pathlib.Path(lims_template_file)
+        super().__init__(**kwargs)
+        if not kwargs.get("export_file_name"):
+            self._export_file_name = f"{self._lims_template_file.stem}_qc_result.xlsx"
+
+    @staticmethod
+    def get_exporter_description() -> str:
+        return "Export qc result from LIMS as DV template data"
+
+    def _export(self, data_holder: PolarsDataHolder) -> None:
+        df = pl.read_csv(
+            self._lims_template_file,
+            separator="\t",
+            encoding="cp1252",
+            infer_schema_length=None,
+        )
+
+        df.write_excel(f"{self._lims_template_file.stem}.xlsx")
+        df = openpyxl.load_workbook(f"{self._lims_template_file.stem}.xlsx")
+        tab = df["Sheet1"]
+
+        parameter_to_column = {}
+        for col in tab.iter_cols(min_row=1, max_row=1):
+            for cell in col:
+                if cell.value:
+                    parameter_to_column[cell.value] = cell.column_letter
+
+        print("Dynamisk mappning:")
+        print(parameter_to_column)
+
+        unique_parameters = sorted(
+            {
+                row["parameter"]
+                for row in data_holder.data.iter_rows(named=True)
+                if row["parameter"] in parameter_to_column
+            },
+            key=lambda p: openpyxl.utils.column_index_from_string(parameter_to_column[p]),
+            reverse=True,
+        )
+        #
+        for parameter in unique_parameters:
+            param_column = parameter_to_column[parameter]
+            param_col_index = openpyxl.utils.column_index_from_string(param_column)
+            total_qc_col_index = param_col_index + 1
+
+            tab.insert_cols(total_qc_col_index)
+            total_qc_column = get_column_letter(total_qc_col_index)
+
+            tab[f"{total_qc_column}1"] = f"TOTAL_QC_{parameter}"
+
+            for row in data_holder.data.iter_rows(named=True):
+                if row["parameter"] == parameter:
+                    rad = row["row_number"]
+                    excel_row = int(rad) + 1
+                    cell_ref = f"{total_qc_column}{excel_row}"
+
+                    tab[cell_ref] = row["TOTAL_QC"]
+
+                    if row.get("total_automatic_info"):
+                        cell = tab[cell_ref]
+                        cell.comment = Comment(
+                            text=row["total_automatic_info"], author="SHARKadm QC Tool"
+                        )
+
+        all_parameters = {
+            row["parameter"] for row in data_holder.data.iter_rows(named=True)
+        }
+        missing_parameters = all_parameters - parameter_to_column.keys()
+        if missing_parameters:
+            print(
+                f"Varning: Följande parametrar hittades inte i Excel-filen: "
+                f"{missing_parameters}"
+            )
+
+        df.save(self.export_file_path)
         print("TOTAL_QC-kolumner har lagts till och fyllts i Excel-filen.")
